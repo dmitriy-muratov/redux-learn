93769ae0002ba25bbb8e870a20944fbdfc02442e
Dmitriy Muratov <dmitriy.muratov@itomy.ch>
2018-01-16 12:00:36 +0200

    get items list with ngrx store

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -2,7 +2,7 @@ import * as ShoppingListActions from './shopping-list.actions';
 import {Ingredient} from '../../shared/ingredient.model';
 
 const initialState = {
-  ingridients: [
+  ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ]
@@ -13,7 +13,7 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingridients, action.payload]
+        ingredients: [...state.ingredients, action.payload]
       };
     default:
       return state;
