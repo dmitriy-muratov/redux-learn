4502cbacd230fdf75065da551e5169bb9687978d
Dmitriy Muratov <dmitriy.muratov@itomy.ch>
2018-01-16 17:48:31 +0200

    ngrx store integration in components;

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs/Subscription';
+import {Component, OnInit} from '@angular/core';
 
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
@@ -11,29 +10,18 @@ import {Observable} from 'rxjs/Observable';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit, OnDestroy {
+export class ShoppingListComponent implements OnInit {
   shoppingListState: Observable<{ingredients: Ingredient[]}>;
-  private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService,
-              private store: Store<{ shoppingList: {ingredients: Ingredient[]} }>) {
-  }
+    constructor(private slService: ShoppingListService,
+                private store: Store<{ shoppingList: {ingredients: Ingredient[]} }>) {
+    }
 
   ngOnInit() {
     this.shoppingListState = this.store.select('shoppingList');
-    // this.subscription = this.slService.ingredientsChanged
-    //   .subscribe(
-    //     (ingredients: Ingredient[]) => {
-    //       this.ingredients = ingredients;
-    //     }
-    //   );
   }
 
   onEditItem(index: number) {
     this.slService.startedEditing.next(index);
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
